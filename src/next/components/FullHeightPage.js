33c136bb5edd46eeacb1f2b69cbf35bf593a934c
Kavin Varnan <skavinvarnan@gmail.com>
2020-06-16 12:26:55 +0530

    Made signup page to work

---
+++ src/next/components/FullHeightPage.js
@@ -0,0 +1,15 @@
+export default ({ children }) => (
+  <div>
+    {children}
+    <style global jsx>{`
+      html,
+      body,
+      body > div:first-child,
+      div#__next,
+      div#__next > div,
+      div#__next > div > div {
+        height: 100%;
+      }
+    `}</style>
+  </div>
+)
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,18 @@
 import SignUpPage from '../next/pages/SignUpPage'
+import FullHeightPage from "../next/components/FullHeightPage";
 
 export default function () {
   return <div>
-    <SignUpPage/>
+    <FullHeightPage>
+      <SignUpPage/>
+    </FullHeightPage>
   </div>
+}
+
+export async function getServerSideProps(context) {
+  // context.res.statusCode = 302
+  // context.res.setHeader('Location', `/`)
+  return {
+    props: {} // will be passed to the page component as props
+  }
 }
